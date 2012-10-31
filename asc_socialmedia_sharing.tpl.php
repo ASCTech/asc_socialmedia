ac9940bfd0c94250f90015e3a820f3787b398c87
Mike Butsko <butsko.7@osu.edu>
2012-10-31 13:37:02 -0400

    no more debug

--- asc_socialmedia_sharing.tpl.php
+++ asc_socialmedia_sharing.tpl.php
@@ -2,7 +2,7 @@
   <div id="asc-sharing-button">share <span id="plus">+</span></div>
   <div id="asc-sharing-icons">
   <a href="http://www.facebook.com/plugins/like.php?href=<?php print urlencode($_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]) ?>" alt="Share on Facebook" id="asc-facebook-share" class="asc-sharing-link" title="Share on Facebook" /></a>
-  <a href="http://twitter.com/share?text=<?php dsm($variables); ?>" alt="Share on Twitter" id="asc-twitter-share" class="asc-sharing-link" title="Share on Twitter" /></a>
+  <a href="http://twitter.com/share?text=" alt="Share on Twitter" id="asc-twitter-share" class="asc-sharing-link" title="Share on Twitter" /></a>
   <a href="" alt="Share on Pinterest" id="asc-pinterest-share" class="asc-sharing-link" title="Share on Pinterest" /></a>
   <a href="" alt="Share on Tumblr" id="asc-tumblr-share" class="asc-sharing-link" title="Share on Tumblr" /></a>
   <a href="" alt="Share on Blogger" id="asc-blogger-share" class="asc-sharing-link" title="Share on Blogger" /></a>
