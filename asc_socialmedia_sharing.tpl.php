400d986423e63611ed03e9fa47bde29bd48bc15a
Mike Butsko <butsko.7@osu.edu>
2012-11-02 13:18:12 -0400

    text not title

--- asc_socialmedia_sharing.tpl.php
+++ asc_socialmedia_sharing.tpl.php
@@ -3,7 +3,7 @@
   <div id="asc-sharing-button">share <div id="plus">+</div></div>
   <div id="asc-sharing-icons">
   <a href="http://www.facebook.com/sharer.php?u=http%3A%2F%2F<?php print $variables['url'] ?>&t=<?php print $variables['title'] ?>" id="asc-facebook-share" class="asc-sharing-link" title="Share on Facebook" target="_blank" /></a>
-  <a href="http://twitter.com/share?url=<?php print $variables['url'] ?>&title=<?php print $variables['title'] ?>" id="asc-twitter-share" class="asc-sharing-link" title="Share on Twitter" target="_blank"/></a>
+  <a href="http://twitter.com/share?url=<?php print $variables['url'] ?>&text=<?php print $variables['title'] ?>" id="asc-twitter-share" class="asc-sharing-link" title="Share on Twitter" target="_blank"/></a>
   <!-- <a href="http://pinterest.com/pin/create/button/?url=<?php print $variables['url'] ?>&media=<?php print $vars['picture'] ?>&description=<?php print $vars['title'] ?>" title="Pin It" id="asc-pinterest-share" class="asc-sharing-link" target="_blank"></a> --> 
   <a href="http://www.tumblr.com/share/link?url=<?php print $variables['url'] ?>&name=<?php print $variables['title'] ?>&description=<?php print $variables['description'] ?>" id="asc-tumblr-share" class="asc-sharing-link" title="Share on Tumblr" target="_blank"></a>
   <!-- <a href="" id="asc-blogger-share" class="asc-sharing-link" title="Share on Blogger" target="_blank"></a> -->
