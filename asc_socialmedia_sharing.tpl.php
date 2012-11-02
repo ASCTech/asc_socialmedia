1d59a2dfe5a4d37ea73e82ff24f98221bf228af4
Mike Butsko <butsko.7@osu.edu>
2012-11-02 11:06:13 -0400

    Working links for several services

--- asc_socialmedia_sharing.tpl.php
+++ asc_socialmedia_sharing.tpl.php
@@ -1,12 +1,15 @@
 <div id="asc-sharing-wrapper">
   <div id="asc-sharing-button">share <div id="plus">+</div></div>
   <div id="asc-sharing-icons">
-  <a href="http://www.facebook.com/plugins/like.php?href=<?php print urlencode($_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]) ?>" alt="Share on Facebook" id="asc-facebook-share" class="asc-sharing-link" title="Share on Facebook" /></a>
-  <a href="http://twitter.com/share?text=" alt="Share on Twitter" id="asc-twitter-share" class="asc-sharing-link" title="Share on Twitter" /></a>
-  <a href="" alt="Share on Pinterest" id="asc-pinterest-share" class="asc-sharing-link" title="Share on Pinterest" /></a>
-  <a href="" alt="Share on Tumblr" id="asc-tumblr-share" class="asc-sharing-link" title="Share on Tumblr" /></a>
-  <a href="" alt="Share on Blogger" id="asc-blogger-share" class="asc-sharing-link" title="Share on Blogger" /></a>
-  <a href="" alt="Share on LinkedIn" id="asc-linkedin-share" class="asc-sharing-link" title="Share on LinkedIn" /></a>
-  <a href="" alt="Share on Google Plus" id="asc-googleplus-share" class="asc-sharing-link" title="Share on Google Plus" /></a>
+  <a href="http://www.facebook.com/sharer.php?u=http%3A%2F%2F<?php print $variables['url'] ?>&t=<?php print $variables['title'] ?>" id="asc-facebook-share" class="asc-sharing-link" title="Share on Facebook" target="_blank" /></a>
+  <a href="http://twitter.com/share?url=<?php print $variables['url'] ?>?title=<?php print $variables['title'] ?>" id="asc-twitter-share" class="asc-sharing-link" title="Share on Twitter" target="_blank"/></a>
+  <!-- <a href="http://pinterest.com/pin/create/button/?url=<?php print $variables['url'] ?>&media=<?php print $vars['picture'] ?>&description=<?php print $vars['title'] ?>" title="Pin It" id="asc-pinterest-share" class="asc-sharing-link" target="_blank"></a> --> 
+  <a href="http://www.tumblr.com/share/link?url=<?php print $variables['url'] ?>&name=<?php print $variables['title'] ?>&description=<?php print $variables['description'] ?>" id="asc-tumblr-share" class="asc-sharing-link" title="Share on Tumblr" target="_blank"></a>
+  <!-- <a href="" id="asc-blogger-share" class="asc-sharing-link" title="Share on Blogger" target="_blank"></a> -->
+  <a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php print $variables['url'] ?>&title=<?php print $variables['title'] ?>&summary=<?php print $variables['description'] ?>" id="asc-linkedin-share" class="asc-sharing-link" title="Share on LinkedIn" target="_blank"></a>
+  <a href="https://plus.google.com/share?url=<?php print $variables['url'] ?>" id="asc-googleplus-share" class="asc-sharing-link" title="Share on Google Plus" target="_blank"></a>
   </div>
 </div>
+<div id="asc-printemail">
+  <a id="asc-print-share" href=""></a>
+  <a id="asc-mail-share" href="mailto:?subject=Link&body=" 
