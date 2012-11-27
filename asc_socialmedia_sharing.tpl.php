0b8ce5e877574beae7583dfad1af2434f25ad2d5
Mike Butsko <butsko.7@osu.edu>
2012-11-27 14:57:19 -0500

    can't target new window

--- asc_socialmedia_sharing.tpl.php
+++ asc_socialmedia_sharing.tpl.php
@@ -2,12 +2,12 @@
 <div id="asc-sharing-dropdown" style="float: left;">
   <div id="asc-sharing-button">share <div id="plus">+</div></div>
   <div id="asc-sharing-icons">
-  <a href="http://www.facebook.com/sharer.php?u=<?php print $variables['url'] ?>&t=<?php print $variables['title'] ?>" id="asc-facebook-share" class="asc-sharing-link" title="Share on Facebook" target="_blank" />Share on Facebook</a>
-  <a href="http://twitter.com/share?url=<?php print $variables['url'] ?>&text=<?php print $variables['title'] ?>" id="asc-twitter-share" class="asc-sharing-link" title="Share on Twitter" target="_blank"/>Share on Twitter</a>
-  <a href="http://www.tumblr.com/share/link?url=<?php print $variables['url'] ?>&name=<?php print $variables['title'] ?>&description=<?php print $variables['description'] ?>" id="asc-tumblr-share" class="asc-sharing-link" title="Share on Tumblr" target="_blank">Share on Tumblr</a>
-  <!-- <a href="" id="asc-blogger-share" class="asc-sharing-link" title="Share on Blogger" target="_blank"></a> -->
-  <a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php print $variables['url'] ?>&title=<?php print $variables['title'] ?>&summary=<?php print $variables['description'] ?>" id="asc-linkedin-share" class="asc-sharing-link" title="Share on LinkedIn" target="_blank">Share on LinkedIn</a>
-  <a href="https://plus.google.com/share?url=<?php print $variables['url'] ?>" id="asc-googleplus-share" class="asc-sharing-link" title="Share on Google Plus" target="_blank">Share on Google Plus</a>
+  <a href="http://www.facebook.com/sharer.php?u=<?php print $variables['url'] ?>&t=<?php print $variables['title'] ?>" id="asc-facebook-share" class="asc-sharing-link" title="Share on Facebook" />Share on Facebook</a>
+  <a href="http://twitter.com/share?url=<?php print $variables['url'] ?>&text=<?php print $variables['title'] ?>" id="asc-twitter-share" class="asc-sharing-link" title="Share on Twitter"/>Share on Twitter</a>
+  <a href="http://www.tumblr.com/share/link?url=<?php print $variables['url'] ?>&name=<?php print $variables['title'] ?>&description=<?php print $variables['description'] ?>" id="asc-tumblr-share" class="asc-sharing-link" title="Share on Tumblr">Share on Tumblr</a>
+  <!-- <a href="" id="asc-blogger-share" class="asc-sharing-link" title="Share on Blogger"></a> -->
+  <a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php print $variables['url'] ?>&title=<?php print $variables['title'] ?>&summary=<?php print $variables['description'] ?>" id="asc-linkedin-share" class="asc-sharing-link" title="Share on LinkedIn">Share on LinkedIn</a>
+  <a href="https://plus.google.com/share?url=<?php print $variables['url'] ?>" id="asc-googleplus-share" class="asc-sharing-link" title="Share on Google Plus">Share on Google Plus</a>
   </div>
 </div>
 <div id="asc-sharing-sharprint">
